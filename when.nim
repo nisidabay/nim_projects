2652a9d85434f86cdb5e5d1804ec9a0238312820
nisidabay <nisidabay@gmail.com>
2026-01-11 10:10:57 +0100

    Modify when.nim

--- when.nim
+++ when.nim
@@ -1,10 +1,7 @@
-# The "when" statement is like "if," but it has these special rules: It uses
- # only simple answers (like numbers or fixed choices) and doesn't start a new
- # place where you can use different names for things. Only one of its answers
- # can be true at the same time, so the computer only does what that answer
- # says. This is helpful if you need to follow different instructions on
- # computers with different features.
- #
+# Nim
+#
+# The "when" statement is like "if.
+
 when system.hostOS == "windows":
   echo "running on Windows!"
 elif system.hostOS == "linux":
